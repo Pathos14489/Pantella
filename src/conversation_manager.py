80f3902b623656500a370c9784f106ea6a02067c
Pathos <administrator@foalcon.com>
2024-01-11 05:10:29 -0800

    tweaked conversation manager and abstracted check_joiner to method

--- src/conversation_manager.py
+++ src/conversation_manager.py
@@ -48,9 +48,9 @@ class conversation_manager():
             self.chat_manager.process_response(player_name, self.config, sentence_queue, input_text, messages, synthesizer, characters, radiant_dialogue, event), 
             self.chat_manager.send_response(sentence_queue, event)
         )
-        messages, _ = results
+        msgs, _ = results
 
-        return messages
+        return msgs
 
     def check_mcm_mic_status(self):
         # Check if the mic setting has been configured in MCM
@@ -110,27 +110,8 @@ class conversation_manager():
         self.say_goodbye = False
         self.in_conversation = True
 
-    def get_if_new_character_joined(self):
-        # check if new character has been added to conversation and switch to Single Prompt Multi-NPC conversation if so
-        num_characters_selected = self.game_state_manager.load_ingame_actor_count()
-        if num_characters_selected > self.character_manager.active_character_count():
-            return True
-        else:
-            return False
-
-    def step(self): # process player input and NPC response until conversation ends at each step of the conversation
-        if self.in_conversation == False:
-            logging.info('Cannot step through conversation when not in conversation')
-            return
-        logging.info('Stepping through conversation...')
-        logging.info(f"Messages: {self.messages}")
-        
-        conversation_ended = self.game_state_manager.load_conversation_ended() # wait for the game to indicate that the conversation has ended or not
+    def check_new_joiner(self):
         new_character_joined = self.get_if_new_character_joined() # check if new character has been added to conversation and switch to Single Prompt Multi-NPC conversation if so
-        
-        radiant_dialogue = self.game_state_manager.load_radiant_dialogue() # check if radiant dialogue is being used
-        conversation_started_radiant = radiant_dialogue # to check later on if conversation started as radiant dialogue
-
         if new_character_joined: # check if new character has been added to conversation and switch to Single Prompt Multi-NPC conversation if so
             try: # load character info, location and other gamestate data when data is available - Starts watching the _mantella_ files in the Skyrim folder and waits for the player to select an NPC
                 character_info, location, in_game_time, is_generic_npc, self.player_name, player_race, player_gender, radiant_dialogue = self.game_state_manager.load_game_state(self.config, self.character_df)
@@ -169,66 +150,106 @@ class conversation_manager():
 
             self.messages = new_context.copy() 
             self.game_state_manager.write_game_info('_mantella_character_selection', 'True') # write to _mantella_character_selection.txt to indicate that the character has been successfully selected to the game
+
+    def get_if_new_character_joined(self):
+        # check if new character has been added to conversation and switch to Single Prompt Multi-NPC conversation if so
+        num_characters_selected = self.game_state_manager.load_ingame_actor_count()
+        if num_characters_selected > self.character_manager.active_character_count():
+            return True
+        else:
+            return False
+
+    def step(self): # process player input and NPC response until conversation ends at each step of the conversation
+        if self.in_conversation == False:
+            logging.info('Cannot step through conversation when not in conversation')
+            return
+        logging.info('Stepping through conversation...')
+        logging.info(f"Messages: {self.messages}")
         
-        # if there are still active characters in the conversation or not radiant dialogue, continue conversation
-        if (self.character_manager.active_character_count() > 1) or (not radiant_dialogue):
-            # if the conversation was initially radiant but now the player is jumping in, reset the system prompt to include player
-            if (not radiant_dialogue) and conversation_started_radiant:
-                conversation_started_radiant = False
-                messages_wo_system_prompt = self.messages[1:]
+        conversation_ended = self.game_state_manager.load_conversation_ended() # wait for the game to indicate that the conversation has ended or not
+        
+        radiant_dialogue = self.game_state_manager.load_radiant_dialogue() # check if radiant dialogue is being used
+        conversation_started_radiant = radiant_dialogue # to check later on if conversation started as radiant dialogue
+        
+        self.check_new_joiner() # check if new character has been added to conversation and switch to Single Prompt Multi-NPC conversation if so
+        
+        
+        if (self.character_manager.active_character_count() <= 0) or radiant_dialogue: # if there are no active characters in the conversation and radiant dialogue is not being used, end the conversation
+            return
+        
+        # if the conversation was initially radiant but now the player is jumping in, reset the system prompt to include player
+        if (not radiant_dialogue) and conversation_started_radiant:
+            conversation_started_radiant = False
+            # messages_wo_system_prompt = self.messages[1:]
 
-            # check if radiant dialogue has switched to multi NPC
-            radiant_dialogue = self.game_state_manager.load_radiant_dialogue()
-            
-            transcript_cleaned = ''
-            transcribed_text = None
-            if not conversation_ended: # check if conversation has ended, if not, get next player input
-                transcribed_text = self.transcriber.get_player_response() # radiant_dialogue and say_goodbye removed
+        # check if radiant dialogue has switched to multi NPC
+        radiant_dialogue = self.game_state_manager.load_radiant_dialogue()
+        
+        transcript_cleaned = ''
+        transcribed_text = None
+        if not conversation_ended: # check if conversation has ended, if not, get next player input
+            transcribed_text = self.transcriber.get_player_response() # radiant_dialogue and say_goodbye removed
 
-                self.game_state_manager.write_game_info('_mantella_player_input', transcribed_text)
+            self.game_state_manager.write_game_info('_mantella_player_input', transcribed_text) # write player input to _mantella_player_input.txt
 
-                transcript_cleaned = utils.clean_text(transcribed_text)
+            transcript_cleaned = utils.clean_text(transcribed_text)
 
-                # if multi NPC conversation, add "Player:" to beginning of output to clarify to the LLM who is speaking
-                # if (self.character_manager.active_character_count() > 1) and not radiant_dialogue:
-                #     transcribed_text = 'Player: ' + transcribed_text
-                # add in-game events to player's response
-                transcribed_text = self.game_state_manager.update_game_events(transcribed_text) # TODO: Switch to using system messages to update ingame events that are generic, and link all ingame events that relate to a character to the character's messages
-                logging.info(f"Text passed to NPC: {transcribed_text}")
+            # if multi NPC conversation, add "Player:" to beginning of output to clarify to the LLM who is speaking
+            # if (self.character_manager.active_character_count() > 1) and not radiant_dialogue:
+            #     transcribed_text = 'Player: ' + transcribed_text
+            # add in-game events to player's response
+            transcribed_text = self.game_state_manager.update_game_events(transcribed_text) # TODO: Switch to using system messages to update ingame events that are generic, and link all ingame events that relate to a character to the character's messages
+            logging.info(f"Text passed to NPC: {transcribed_text}")
 
-            # check if conversation has ended again after player input
-            with open(f'{self.config.game_path}/_mantella_end_conversation.txt', 'r', encoding='utf-8') as f:
-                conversation_ended = f.readline().strip()
+        # check if conversation has ended again after player input
+        with open(f'{self.config.game_path}/_mantella_end_conversation.txt', 'r', encoding='utf-8') as f:
+            conversation_ended = f.readline().strip()
+        
+        self.check_new_joiner() # check if new character has been added to conversation and switch to Single Prompt Multi-NPC conversation if so after player input
 
-            # check if user is ending conversation
-            if (self.transcriber.activation_name_exists(transcript_cleaned, self.config.end_conversation_keyword.lower())) or (self.transcriber.activation_name_exists(transcript_cleaned, 'good bye')) or (self.transcriber.activation_name_exists(transcript_cleaned, 'goodbye')) or (conversation_ended.lower() == 'true'):
-                self.game_state_manager.end_conversation(conversation_ended, self, self.tokenizer, self.synthesizer, self.chat_manager, self.messages, self.character_manager.active_characters, self.tokens_available, character)
-                self.in_conversation = False
-                return
+        # check if user is ending conversation
+        if (self.transcriber.activation_name_exists(transcript_cleaned, self.config.end_conversation_keyword.lower())) or (self.transcriber.activation_name_exists(transcript_cleaned, 'good bye')) or (self.transcriber.activation_name_exists(transcript_cleaned, 'goodbye')) or (conversation_ended.lower() == 'true'):
+            # Detect who the player is talking to
+            names = []
+            for character in self.character_manager.active_characters.values():
+                character_names = character.name.split(' ')
+                names.append(character_names)
+            all_words = transcript_cleaned.split(' ')
+            goodbye_target_character = None
+            for word in all_words:
+                for name_group in names:
+                    if word in name_group:
+                        goodbye_target_character = self.character_manager.active_characters[' '.join(name_group)]
+                        break
+            if goodbye_target_character == None:
+                goodbye_target_character = self.chat_manager.active_character # If we can't figure out who the player is talking to, just assume it's the active character
+            self.game_state_manager.end_conversation(conversation_ended, self, self.tokenizer, self.synthesizer, self.chat_manager, self.messages, self.character_manager.active_characters, self.tokens_available, goodbye_target_character)
+            self.in_conversation = False
+            return
 
-            # Let the player know that they were heard
-            #audio_file = synthesizer.synthesize(character.info['voice_model'], character.info['skyrim_voice_folder'], 'Beep boop. Let me think.')
-            #chat_manager.save_files_to_voice_folders([audio_file, 'Beep boop. Let me think.'])
+        # Let the player know that they were heard
+        #audio_file = synthesizer.synthesize(character.info['voice_model'], character.info['skyrim_voice_folder'], 'Beep boop. Let me think.')
+        #chat_manager.save_files_to_voice_folders([audio_file, 'Beep boop. Let me think.'])
 
-            # check if NPC is in combat to change their voice tone (if one on one conversation)
-            if self.character_manager.active_character_count() == 1:
-                aggro = self.game_state_manager.load_data_when_available('_mantella_actor_is_in_combat', '').lower()
-                if aggro == 'true':
-                    self.chat_manager.active_character.is_in_combat = 1
-                else:
-                    self.chat_manager.active_character.is_in_combat = 0
+        # check if NPC is in combat to change their voice tone (if one on one conversation)
+        if self.character_manager.active_character_count() == 1:
+            aggro = self.game_state_manager.load_data_when_available('_mantella_actor_is_in_combat', '').lower()
+            if aggro == 'true':
+                self.chat_manager.active_character.is_in_combat = 1
+            else:
+                self.chat_manager.active_character.is_in_combat = 0
 
-            # get character's response
-            if transcribed_text: # If the player said something, add it to the messages
-                if self.character_manager.active_character_count() > 1: # if multi NPC conversation
-                    self.messages = asyncio.run(self.get_response(self.player_name, transcribed_text, self.messages, self.synthesizer, self.character_manager, radiant_dialogue))
-                else: # if single NPC conversation
-                    perspective_player_name, perspective_player_desc, trust = self.character_manager.active_character.get_perspective_player_identity()
-                    self.messages = asyncio.run(self.get_response(perspective_player_name, transcribed_text, self.messages, self.synthesizer, self.character_manager, radiant_dialogue))
+        # get character's response
+        if transcribed_text: # If the player said something, add it to the messages
+            if self.character_manager.active_character_count() > 1: # if multi NPC conversation
+                self.messages = asyncio.run(self.get_response(self.player_name, transcribed_text, self.messages, self.synthesizer, self.character_manager, radiant_dialogue))
+            else: # if single NPC conversation
+                perspective_player_name, perspective_player_desc, trust = self.chat_manager.active_character.get_perspective_player_identity()
+                self.messages = asyncio.run(self.get_response(perspective_player_name, transcribed_text, self.messages, self.synthesizer, self.character_manager, radiant_dialogue))
 
-            # if the conversation is becoming too long, save the conversation to memory and reload
-            current_conversation_limit_pct = self.config.conversation_limit_pct # TODO: Come up with a smarter way of deciding when to reload a conversation
-            if self.tokenizer.num_tokens_from_messages(self.messages[1:]) > (round(self.tokens_available*current_conversation_limit_pct,0)): 
-                _, context, self.messages = self.game_state_manager.reload_conversation(self, self.tokenizer, self.synthesizer, self.chat_manager, self.messages, self.character_manager.active_characters, self.tokens_available, self.token_limit, location, in_game_time)
-                # continue conversation
-                self.messages = asyncio.run(self.get_response(self.player_name, f"{character.name}?", context, self.synthesizer, self.character_manager, radiant_dialogue))
+        # if the conversation is becoming too long, save the conversation to memory and reload
+        current_conversation_limit_pct = self.config.conversation_limit_pct # TODO: Come up with a smarter way of deciding when to reload a conversation
+        if self.tokenizer.num_tokens_from_messages(self.messages[1:]) > (round(self.tokens_available*current_conversation_limit_pct,0)): 
+            _, context, self.messages = self.game_state_manager.reload_conversation(self, self.tokenizer, self.synthesizer, self.chat_manager, self.messages, self.character_manager.active_characters, self.tokens_available, self.token_limit, location, in_game_time)
+            # continue conversation
+            self.messages = asyncio.run(self.get_response(self.player_name, f"{character.name}?", context, self.synthesizer, self.character_manager, radiant_dialogue))
