5c3143991528ff45b3a88f2af6f94976511e6aeb
Pathos <administrator@foalcon.com>
2024-01-18 10:55:09 -0800

    updated game_manager

--- src/game_manager.py
+++ src/game_manager.py
@@ -9,8 +9,9 @@ class CharacterDoesNotExist(Exception):
 
 
 class GameStateManager:
-    def __init__(self, game_path):
-        self.game_path = game_path
+    def __init__(self, conversation_manager):
+        self.conversation_manager = conversation_manager
+        self.game_path = self.conversation_manager.config.game_path
         self.prev_game_time = ''
 
 
@@ -175,12 +176,12 @@ class GameStateManager:
         return character_name, character_id, location, in_game_time, player_name, player_race, player_gender, radiant_dialogue
     
     
-    def load_unnamed_npc(self, character_name, character_df):
+    def load_unnamed_npc(self, character_name):
         """Load generic NPC if character cannot be found in skyrim_characters.csv"""
 
-        male_voice_models = character_df.male_voice_models
-        female_voice_models = character_df.female_voice_models
-        voice_model_ids = character_df.voice_model_ids
+        male_voice_models = self.conversation_manager.character_database.male_voice_models
+        female_voice_models = self.conversation_manager.character_database.female_voice_models
+        voice_model_ids = self.conversation_manager.character_database.voice_model_ids
 
         actor_voice_model = self.load_data_when_available('_mantella_actor_voice', '')
         actor_voice_model_id = actor_voice_model.split('(')[1].split(')')[0]
@@ -200,7 +201,7 @@ class GameStateManager:
         
         # if voice_model not found in the voice model ID list
         if voice_model == '':
-            voice_model = character_df.get_character_by_voice_folder(actor_voice_model_name)["voice_model"] # return voice model from actor_voice_model_name
+            voice_model = self.conversation_manager.character_database.get_character_by_voice_folder(actor_voice_model_name)["voice_model"] # return voice model from actor_voice_model_name
         else:    
             if actor_sex == '1':
                 try:
@@ -217,7 +218,7 @@ class GameStateManager:
                 except:
                     voice_model = 'Male '+actor_race # Default to Same Sex Racial Equivalent
 
-        skyrim_voice_folder = character_df.get_voice_folder_by_voice_model(voice_model)
+        skyrim_voice_folder = self.conversation_manager.character_database.get_voice_folder_by_voice_model(voice_model)
         
         character_info = {
             'name': character_name, # TODO: Generate random names for generic NPCs and figure out how to apply them in-game
@@ -228,7 +229,7 @@ class GameStateManager:
 
         # TODO: Enable this after adding random name generation to generic NPCs, otherwise all generic NPCs will share the same info I think
         # (Example: All Bandits would see themselves as Male Nord Bandits if the first Bandit you talked to was a Male Nord Bandit)
-        # character_df.patch_character_info(character_info) # Add character info to skyrim_characters json directory if using json mode
+        # character_database.patch_character_info(character_info) # Add character info to skyrim_characters json directory if using json mode
 
         return character_info
     
@@ -243,11 +244,11 @@ class GameStateManager:
         return int(in_game_time)
     
     @utils.time_it
-    def load_game_state(self, config, character_df):
+    def load_game_state(self):
         """Load game variables from _mantella_ files in Skyrim folder (data passed by the Mantella spell)"""
 
-        if config.debug_mode == '1':
-            character_name, character_id, location, in_game_time, player_name, player_race, player_gender = self.debugging_setup(config.debug_character_name)
+        if self.conversation_manager.config.debug_mode == '1':
+            character_name, character_id, location, in_game_time, player_name, player_race, player_gender = self.debugging_setup(self.conversation_manager.config.debug_character_name)
         else:
             location = self.get_current_location()
             in_game_time = self.get_current_game_time()
@@ -261,18 +262,20 @@ class GameStateManager:
         player_race = self.load_player_race() # get the player's race from _mantella_player_race.txt
         player_gender = self.load_player_gender() # get player's gender from _mantella_player_gender.txt
         radiant_dialogue = self.load_radiant_dialogue() # get the radiant dialogue setting from _mantella_radiant_dialogue.txt
-
-        try: # load character from skyrim_characters json directory
-            character_info = character_df.named_index[character_name]
+        
+        # TODO: Improve character lookup to be more accurate and to include generating character stats inspired by their generic name for generic NPCs instead of leaving them generic.
+        # (example: make a backstory for a Bandit because the NPC was named Bandit, then generate a real name, and background inspired by that vague name for use in-corversation)
+        try: # load character from skyrim_characters json directory 
+            character_info = self.conversation_manager.character_database.named_index[character_name]
             is_generic_npc = False
         except KeyError: # character not found
             try: # try searching by ID
                 logging.info(f"Could not find {character_name} in skyrim_characters.csv. Searching by ID {character_id}...")
-                character_info = character_df.baseid_int_index[character_id]
+                character_info = self.conversation_manager.character_database.baseid_int_index[character_id]
                 is_generic_npc = False
             except KeyError:
                 logging.info(f"NPC '{character_name}' could not be found in 'skyrim_characters.csv'. If this is not a generic NPC, please ensure '{character_name}' exists in the CSV's 'name' column exactly as written here, and that there is a voice model associated with them.")
-                character_info = self.load_unnamed_npc(character_name, character_df)
+                character_info = self.load_unnamed_npc(character_name)
                 is_generic_npc = True
 
         location = self.get_current_location(location) # Check if location has changed since last check
@@ -282,6 +285,9 @@ class GameStateManager:
         actor_voice_model = self.load_data_when_available('_mantella_actor_voice', '')
         actor_voice_model_name = actor_voice_model.split('<')[1].split(' ')[0]
         character_info['in_game_voice_model'] = actor_voice_model_name
+        character_info['character_id'] = character_id
+        character_info['character_name'] = character_name
+        character_info['in_game_voice_model_id'] = actor_voice_model.split('(')[1].split(')')[0]
 
         actor_relationship_rank = self.load_data_when_available('_mantella_actor_relationship', '')
         try:
@@ -295,7 +301,7 @@ class GameStateManager:
 
     
     @utils.time_it
-    def update_game_events(self, conversation_manager):
+    def update_game_events(self):
         """Add in-game events to player's response"""
 
         # append in-game events to player's response
@@ -308,7 +314,7 @@ class GameStateManager:
 
         # Is Player in combat with NPC
         in_combat = self.load_data_when_available('_mantella_actor_is_enemy', '').lower() == 'true' 
-        character = conversation_manager.chat_manager.active_character
+        character = self.conversation_manager.chat_manager.active_character
         perspective_name, perspective_description, trust = character.get_perspective_player_identity()
         if in_combat:
             in_game_events = in_game_events + f'\n*{perspective_description} is fighting {character.name}. This is either because they are enemies or because {perspective_name} attacked {character.name} first.*'
@@ -337,90 +343,67 @@ class GameStateManager:
         
         if len(in_game_events.strip()) > 0:
             logging.info(f'In-game events since previous exchange:\n{in_game_events}')
-            context = conversation_manager.messages.copy()
-            last_message = context[-1]
-            if last_message['role'] == conversation_manager.config.system_name:
-                context[-1]['content'] += "\n" + in_game_events
-            else:
-                context += [{
-                    "role": conversation_manager.config.system_name,
+            if len(self.conversation_manager.messages) == 0: # if there are no messages in the conversation yet, add in-game events to the first message from the system
+                self.conversation_manager.messages += [{
+                    "role": self.conversation_manager.config.system_name,
                     "content": in_game_events,
-                }] # add in-game events to current ongoing conversation
-        else:
-            context = conversation_manager.messages
-
-        return context
+                }]
+            else: # if there are messages in the conversation, add in-game events to the last message from the system
+                last_message = self.conversation_manager.messages[-1]
+                if last_message['role'] == self.conversation_manager.config.system_name: # if last message was from the system, add in-game events to the system message
+                    self.conversation_manager.messages[-1]['content'] += "\n" + in_game_events
+                else: # if last message was from the NPC, add in-game events to the ongoing conversation as a new message from the system
+                    self.conversation_manager.messages += [{
+                        "role": self.conversation_manager.config.system_name,
+                        "content": in_game_events,
+                    }] # add in-game events to current ongoing conversation
     
+    def summarize_all_summaries(self):
+        """Summarize all summaries in the conversation"""
+        summary = None
+        for _, character in self.conversation_manager.character_manager.active_characters.items(): # Get conversation summary from any character in the conversation or generate a new one
+            if summary == None: # If summary has already been generated for another character in a multi NPC conversation (multi NPC memory summaries are shared)
+                summary = character.save_conversation()
+            else: # If summary has not been generated yet, generate it
+                _ = character.save_conversation(summary)
+        return summary
     
     @utils.time_it
-    def end_conversation(self, conversation_ended, conversation_manager, tokenizer, synthesizer, chat_manager, messages, active_characters, tokens_available, character):
+    def end_conversation(self, character):
         """Say final goodbye lines and save conversation to memory"""
 
         # say goodbyes
-        if not conversation_ended: # say line if NPC is not already deactivated
-            latest_character = list(active_characters.items())[-1][1]
-            audio_file = synthesizer.synthesize(latest_character, conversation_manager.config.goodbye_npc_response)
-            chat_manager.save_files_to_voice_folders([audio_file, conversation_manager.config.goodbye_npc_response])
+        if not self.conversation_manager.conversation_ended: # say line if NPC is not already deactivated
+            latest_character = list(self.conversation_manager.character_manager.active_characters.items())[-1][1] # get latest character in conversation
+            latest_character.say(self.conversation_manager.config.goodbye_npc_response+'.') # let the player know that the conversation is ending using the latest character in the conversation that isn't the player to say it
 
         perspective_name, _, _ = character.get_perspective_player_identity() # get perspective name - How the NPC refers to the player
+        self.conversation_manager.messages.append({"role": perspective_name, "content": self.conversation_manager.config.end_conversation_keyword+'.'})
+        self.conversation_manager.messages.append({"role": character.name, "content": self.conversation_manager.config.end_conversation_keyword+'.'})
 
-        messages.append({"role": perspective_name, "content": conversation_manager.config.end_conversation_keyword+'.'})
-        messages.append({"role": character.name, "content": conversation_manager.config.end_conversation_keyword+'.'})
-
-        summary = None
-        for character_name, character in active_characters.items(): # Get conversation summary from any character in the conversation or generate a new one
-            if summary == None: # If summary has already been generated for another character in a multi NPC conversation (multi NPC memory summaries are shared)
-                summary = character.save_conversation(tokenizer, messages, tokens_available, conversation_manager)
-            else: # If summary has not been generated yet, generate it
-                _ = character.save_conversation(tokenizer, messages, tokens_available, conversation_manager, summary)
+        self.summarize_all_summaries() # save conversation to memory
         logging.info('Conversation ended.')
 
         self.write_game_info('_mantella_in_game_events', '') # clear in-game events
         self.write_game_info('_mantella_end_conversation', 'True') # tell Skyrim papyrus script conversation has ended
-        time.sleep(conversation_manager.config.end_conversation_wait_time) # wait a few seconds for everything to register
-
+        time.sleep(self.conversation_manager.config.end_conversation_wait_time) # wait a few seconds for everything to register
         return None
     
-    
     @utils.time_it
-    def reload_conversation(self, conversation_manager, encoding, synthesizer, chat_manager, messages, active_characters, tokens_available, token_limit, location, in_game_time):
+    def reload_conversation(self): 
         """Restart conversation to save conversation to memory when token count is reaching its limit"""
+        logging.info('Reloading conversation...')
+        latest_character = list(self.conversation_manager.character_manager.active_characters.items())[-1][1] # get latest character in conversation
+        latest_character.say(self.conversation_manager.config.collecting_thoughts_npc_response+'.') # let the player know that the conversation is reloading using the latest character in the conversation that isn't the player to say it
 
-        latest_character = list(active_characters.items())[-1][1]
-        # let the player know that the conversation is reloading
-        audio_file = synthesizer.synthesize(latest_character, conversation_manager.config.collecting_thoughts_npc_response)
-        chat_manager.save_files_to_voice_folders([audio_file, conversation_manager.config.collecting_thoughts_npc_response])
+        self.summarize_all_summaries() # save conversation to memory
+        
+        time.sleep(self.config.reload_wait_time) # let the new file register on the system
 
-        messages.append({"role": conversation_manager.config.user_name, "content": latest_character.info['name']+'?'}) # TODO: More robust way of returning to conversation, this is too limited
-        if len(list(active_characters.items())) > 1:
-            collecting_thoughts_response = latest_character.info['name']+': '+conversation_manager.config.collecting_thoughts_npc_response+'.'
-        else:
-            collecting_thoughts_response = conversation_manager.config.collecting_thoughts_npc_response+'.'
-        messages.append({"role": conversation_manager.config.assistant_name, "content": collecting_thoughts_response})
-
-        # save the conversation so far
-        summary = None
-        for character_name, character in active_characters.items():
-            if summary == None:
-                summary = character.save_conversation(encoding, messages, tokens_available, conversation_manager)
-            else:
-                _ = character.save_conversation(encoding, messages, tokens_available, conversation_manager, summary)
-        # let the new file register on the system
-        time.sleep(1)
-        # if a new conversation summary file was created, load this latest file
-        for character_name, character in active_characters.items():
-            conversation_summary_file = character.get_latest_conversation_summary_file_path()
-
-        # reload context
-        keys = list(active_characters.keys())
-        prompt = conversation_manager.config.prompt
-        if len(keys) > 1:
-            prompt = conversation_manager.config.multi_npc_prompt
-        context = latest_character.set_context(prompt, location, in_game_time, active_characters, token_limit,"false")
-
-        # add previous few back and forths from last conversation
-        messages_wo_system_prompt = messages[1:]
-        messages_last_entries = messages_wo_system_prompt[-8:]
-        context.extend(messages_last_entries)
-
-        return conversation_summary_file, context, messages
+        self.conversation_manager.messages = self.conversation_manager.messages[-self.config.reload_buffer:] # Set context to just the last few messages
+        if self.conversation_manager.active_character_count() > 1: # If there are multiple characters in the conversation, add a message from the player to the NPC that was talking last to get the conversation going again
+            self.conversation_manager.messages.append({"role": self.conversation_manager.player_name, "content": latest_character.name+'?'})
+        else: # If there is only one character in the conversation, add a message from the player to the NPC to get the conversation going again
+            perspective_player_name, _, _ = latest_character.get_perspective_player_identity()
+            self.conversation_manager.messages.append({"role": perspective_player_name, "content": latest_character.name+'?'})
+        logging.info('Conversation reloaded -- Resuming conversation...')
