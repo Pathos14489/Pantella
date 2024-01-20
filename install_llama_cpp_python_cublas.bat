6906c38bc3a812a240c182814018d65df614de59
Pathos <administrator@foalcon.com>
2024-01-19 23:26:23 -0800

    made install scripts files intent more clear

--- install_llama_cpp_python_cublas.bat
+++ install_llama_cpp_python_cublas.bat
@@ -15,11 +15,12 @@ for /f "tokens=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 delims= " %%a in ('nvcc --ver
 echo CUDA Version: %CUDA_VERSION%
 echo %CMAKE_ARGS%
 if "%CUDA_VERSION%"=="" (
-    :: run the CPU version
+    :: run the CPU version if CUDA is not installed
     echo CUDA not found, running CPU installer/updater  
     call llama_cpp_python_cpu.bat
 ) else (
-    echo Updating llama-cpp-python
-    python3 -m pip install llama-cpp-python==0.2.28 --no-deps --no-cache-dir --force-reinstall --upgrade
+    echo Installing llama-cpp-python for CUDA
+    python3 -m pip install llama-cpp-python==0.2.28 --no-deps --no-cache-dir --force-reinstall --upgrade 
+    :: 0.2.28 because the latest version broke GPU support temporarily
     echo Done
 )
