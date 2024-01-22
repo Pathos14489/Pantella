7fae17eb53f41c1a2c9e86e112440deab8d4a0c9
Pathos <administrator@foalcon.com>
2024-01-22 13:36:06 -0800

    fixed typo

--- src/tokenizers/koboldcpp.py
+++ src/tokenizers/koboldcpp.py
@@ -2,12 +2,12 @@ import src.utils as utils
 import src.tokenizers.base_tokenizer as tokenizer
 import logging
 import requests
-tokenizer_slug = "kobcoldcpp"
+tokenizer_slug = "koboldcpp"
 class Tokenizer(tokenizer.base_Tokenizer): # Gets token count from OpenAI's embedding API -- WARNING SLOW AS HELL -- Only use if you don't want to set up the right tokenizer for your local model or if you don't know how to do that
     def __init__(self, conversation_manager, client):
         super().__init__(conversation_manager)
         if not (self.config.inference_engine == "openai" or self.config.inference_engine == "default"):
-            logging.error(f"kobcoldcpp tokenizer only works using OpenAI's API! Please check your config.ini file and try again!")
+            logging.error(f"koboldcpp tokenizer only works using OpenAI's API! Please check your config.ini file and try again!")
             input("Press enter to continue...")
             exit()
         self.tokenizer_slug = tokenizer_slug
