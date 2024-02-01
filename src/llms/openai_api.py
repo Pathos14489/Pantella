42a9743d1f6cd4dac2858407807ac0efd5149342
Pathos <administrator@foalcon.com>
2024-02-01 13:14:14 -0800

    moved setting from config to openai_api.py

--- src/llms/openai_api.py
+++ src/llms/openai_api.py
@@ -23,6 +23,11 @@ class LLM(base_LLM.base_LLM):
         super().__init__(conversation_manager)
         self.inference_engine_name = inference_engine_name
         self.tokenizer_slug = "tiktoken" # Fastest tokenizer for OpenAI models, change if you want to use a different tokenizer (use 'embedding' for compatibility with any model using the openai API)
+        
+        # Is LLM Local?
+        self.is_local = True
+        if self.config.alternative_openai_api_base == 'none' or self.config.alternative_openai_api_base == "https://openrouter.ai/api/v1" or self.config.alternative_openai_api_base == "http://openrouter.ai/api/v1":
+            self.is_local = False
 
         llm = self.config.llm
         if llm == 'gpt-3.5-turbo':
@@ -61,8 +66,8 @@ class LLM(base_LLM.base_LLM):
             token_limit = 16_385
         elif llm == 'gpt-4-1106-preview':
             token_limit = 128_000
-        elif self.config.is_local:
-            logging.info(f"Could not find number of available tokens for {llm} for tiktoken. Defaulting to token count of {str(self.config.maximum_local_tokens)} (this number can be changed via the `maximum_local_tokens` setting in config.ini).")
+        elif self.is_local:
+            logging.info(f"Could not find number of available tokens for {llm} for tiktoken. Defaulting to token count of {str(self.config.maximum_local_tokens)} (this number can be changed via the `maximum_local_tokens` setting in config.json).")
             logging.info("WARNING: Tiktoken is being run using the default tokenizer, which is not always correct. If you're using a local model, try using the embedding tokenizer instead if it's supported by your API emulation method. It's slower and might be incompatible with some configurations, but more accurate.")
             token_limit = self.config.maximum_local_tokens # Default to 4096 tokens for local models
         self.config.maximum_local_tokens = token_limit # Set the maximum number of tokens for local models to the number of tokens available for the model chosen    
@@ -79,10 +84,10 @@ class LLM(base_LLM.base_LLM):
             self.client.base_url  = self.config.alternative_openai_api_base
             logging.info(f"Using OpenAI API base: {self.client.base_url}")
 
-        if self.config.is_local:
+        if self.is_local:
             logging.info(f"Running Mantella with local language model")
         else:
-            logging.info(f"Running Mantella with '{self.config.llm}'. The language model chosen can be changed via config.ini")
+            logging.info(f"Running Mantella with '{self.config.llm}'. The language model chosen can be changed via config.json")
     
     @utils.time_it
     def create(self, messages):
