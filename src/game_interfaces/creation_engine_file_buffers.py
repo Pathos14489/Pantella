340ba888027af43d83999f40212985bede2f6a80
Pathos <29135741+Pathos14489@users.noreply.github.com>
2024-05-23 19:36:39 -0700

    fixed backslashes

--- src/game_interfaces/creation_engine_file_buffers.py
+++ src/game_interfaces/creation_engine_file_buffers.py
@@ -18,8 +18,8 @@ class GameInterface(BaseGameInterface):
             self.ready = False
             logging.error(f"Game path does not exist: {self.config.game_path}")
         else:
-            if not os.path.exists(self.config.game_path+'/_mantella_skyrim_folder.txt'):
-                logging.warn(f'''Warning: Could not find _mantella_skyrim_folder.txt in {self.config.game_path}.\nIf you have not yet casted the Mantella spell in-game you can safely ignore this message.\nIf you have casted the Mantella spell please check that your\nMantellaSoftware/config.json "skyrim_folder" has been set correctly\n(instructions on how to set this up are in the config file itself).\nIf you are still having issues, a list of solutions can be found here: \nhttps://github.com/art-from-the-machine/Mantella#issues-qa\n''')
+            if not os.path.exists(self.config.game_path+'\\_mantella_skyrim_folder.txt'):
+                logging.warn(f'''Warning: Could not find _mantella_skyrim_folder.txt in {self.config.game_path}.\nIf you have not yet casted the Mantella spell in-game you can safely ignore this message.\nIf you have casted the Mantella spell please check that your\nMantellaSoftware\\config.json "skyrim_folder" has been set correctly\n(instructions on how to set this up are in the config file itself).\nIf you are still having issues, a list of solutions can be found here: \nhttps://github.com/art-from-the-machine/Mantella#issues-qa\n''')
         if not os.path.exists(self.mod_voice_dir):
             raise FileNotFoundError(f"Mod voice directory not found at {self.mod_voice_dir}")
 
@@ -65,7 +65,7 @@ class GameInterface(BaseGameInterface):
                 wav_file_to_use = self.f4_wav_file2
                 subtitle += " Mantella2"
                 self.f4_use_wav_file1 = True
-            wav_file_path = f"{self.mod_voice_dir}/{wav_file_to_use}"
+            wav_file_path = f"{self.mod_voice_dir}\\{wav_file_to_use}"
             if os.path.exists(wav_file_path):
                 os.remove(wav_file_path)
             shutil.copyfile(audio_file, wav_file_path)
@@ -74,20 +74,20 @@ class GameInterface(BaseGameInterface):
         """Save voice model folder to Mantella Spell if it does not already exist"""
         self.in_game_voice_model = in_game_voice_folder
 
-        in_game_voice_folder_path = f"{self.mod_voice_dir}/{in_game_voice_folder}/"
+        in_game_voice_folder_path = f"{self.mod_voice_dir}\\{in_game_voice_folder}\\"
         if not os.path.exists(in_game_voice_folder_path):
             os.mkdir(in_game_voice_folder_path)
 
             # copy voicelines from one voice folder to this new voice folder
             # this step is needed for Skyrim to acknowledge the folder
-            example_folder = f"{self.mod_voice_dir}/MaleNord/"
+            example_folder = f"{self.mod_voice_dir}\\MaleNord\\"
             for file_name in os.listdir(example_folder):
                 source_file_path = os.path.join(example_folder, file_name)
 
                 if os.path.isfile(source_file_path):
                     shutil.copy(source_file_path, in_game_voice_folder_path)
 
-            self.write_game_info('_mantella_status', 'Error with Mantella.exe. Please check MantellaSoftware/logging.log')
+            self.write_game_info('_mantella_status', 'Error with Mantella.exe. Please check MantellaSoftware\\logging.log')
             logging.warn("Unknown NPC detected. This NPC will be able to speak once you restart Skyrim. To learn how to add memory, a background, and a voice model of your choosing to this NPC, see here: https://github.com/art-from-the-machine/Mantella#adding-modded-npcs")
             input('\nPress any key to exit...')
             sys.exit(0)
@@ -97,10 +97,10 @@ class GameInterface(BaseGameInterface):
         for sub_folder in os.listdir(self.mod_voice_dir):
             try:
                 if self.game_id != "fallout4": # delete both the wav file and lip file if the game isn't Fallout4
-                    os.remove(f"{self.mod_voice_dir}/{sub_folder}/{self.wav_file}")
-                    os.remove(f"{self.mod_voice_dir}/{sub_folder}/{self.lip_file}")
+                    os.remove(f"{self.mod_voice_dir}\\{sub_folder}\\{self.wav_file}")
+                    os.remove(f"{self.mod_voice_dir}\\{sub_folder}\\{self.lip_file}")
                 else: #if the game is Fallout 4 only delete the lip file
-                    os.remove(f"{self.mod_voice_dir}/{sub_folder}/{self.f4_lip_file}")
+                    os.remove(f"{self.mod_voice_dir}\\{sub_folder}\\{self.f4_lip_file}")
             except:
                 continue
         
@@ -119,7 +119,7 @@ class GameInterface(BaseGameInterface):
                 wav_file_to_use = self.f4_wav_file2
                 subtitle += " Mantella2"
                 self.f4_use_wav_file1 = True
-            wav_file_path = f"{self.mod_voice_dir}/{wav_file_to_use}"
+            wav_file_path = f"{self.mod_voice_dir}\\{wav_file_to_use}"
             if os.path.exists(wav_file_path):
                 os.remove(wav_file_path)
             shutil.copyfile(audio_file, wav_file_path)
@@ -133,12 +133,12 @@ class GameInterface(BaseGameInterface):
                 if sub_folder.is_dir():
                     #copy both the wav file and lip file if the game isn't Fallout4
                     if self.game_id !="fallout4":
-                        shutil.copyfile(audio_file, f"{sub_folder.path}/{self.wav_file}")
-                    shutil.copyfile(audio_file.replace(".wav", ".lip"), f"{sub_folder.path}/{self.f4_lip_file}")
+                        shutil.copyfile(audio_file, f"{sub_folder.path}\\{self.wav_file}")
+                    shutil.copyfile(audio_file.replace(".wav", ".lip"), f"{sub_folder.path}\\{self.f4_lip_file}")
         else:
             if self.game_id !="fallout4":
-                shutil.copyfile(audio_file, f"{self.mod_voice_dir}/{self.active_character.in_game_voice_model}/{self.wav_file}")
-            shutil.copyfile(audio_file.replace(".wav", ".lip"), str(f"{self.mod_voice_dir}/{self.active_character.in_game_voice_model}/{self.lip_file}").replace("/", "\\"))
+                shutil.copyfile(audio_file, f"{self.mod_voice_dir}\\{self.active_character.in_game_voice_model}\\{self.wav_file}")
+            shutil.copyfile(audio_file.replace(".wav", ".lip"), str(f"{self.mod_voice_dir}\\{self.active_character.in_game_voice_model}\\{self.lip_file}").replace("/", "\\"))
 
         logging.info(f"{self.active_character.name} should speak")
         if self.character_num == 0:
@@ -165,7 +165,7 @@ class GameInterface(BaseGameInterface):
             #if Fallout4 is running the audio will be sync by checking if say line is set to false because the game can internally check if an audio file has finished playing
             # wait for the audio playback to complete before getting the next file
             if self.game_id == "fallout4":
-                with open(f'{self.root_mod_folter}/_mantella_actor_count.txt', 'r', encoding='utf-8') as f:
+                with open(f'{self.root_mod_folter}\\_mantella_actor_count.txt', 'r', encoding='utf-8') as f:
                     mantellaactorcount = f.read().strip() 
                 # Outer loop to continuously check the files
                 while True:
@@ -173,7 +173,7 @@ class GameInterface(BaseGameInterface):
 
                     # Iterate through the number of files indicated by mantellaactorcount
                     for i in range(1, int(mantellaactorcount) + 1):
-                        file_name = f'{self.root_mod_folter}/_mantella_say_line'
+                        file_name = f'{self.root_mod_folter}\\_mantella_say_line'
                         if i != 1:
                             file_name += f'_{i}'  # Append the file number for files 2 and above
                         file_name += '.txt'
@@ -197,7 +197,7 @@ class GameInterface(BaseGameInterface):
 
         for attempt in range(max_attempts):
             try:
-                with open(f'{self.game_path}/{text_file_name}.txt', 'w', encoding='utf-8') as f:
+                with open(f'{self.game_path}\\{text_file_name}.txt', 'w', encoding='utf-8') as f:
                     f.write(text)
                 break
             except PermissionError:
@@ -212,10 +212,10 @@ class GameInterface(BaseGameInterface):
     def load_data_when_available(self, text_file_name, text = '', callback = None):
         while text == '':
             try:
-                with open(f'{self.game_path}/{text_file_name}.txt', 'r', encoding='utf-8') as f:
+                with open(f'{self.game_path}\\{text_file_name}.txt', 'r', encoding='utf-8') as f:
                     text = f.readline().strip()
             except:
-                with open(f'{self.game_path}/{text_file_name}.txt', 'r', encoding='ansi') as f:
+                with open(f'{self.game_path}\\{text_file_name}.txt', 'r', encoding='ansi') as f:
                     text = f.readline().strip()
             # decrease stress on CPU while waiting for file to populate
             if callback != None:
@@ -261,7 +261,7 @@ class GameInterface(BaseGameInterface):
         self.write_game_info('_mantella_say_line_9', 'False')
         self.write_game_info('_mantella_say_line_10', 'False')
         self.write_game_info('_mantella_actor_count', '0')
-        if not os.path.exists(f'{self.game_path}/_mantella_context_string.txt'):
+        if not os.path.exists(f'{self.game_path}\\_mantella_context_string.txt'):
             self.write_game_info('_mantella_context_string', '')
 
         self.write_game_info('_mantella_player_input', '')
@@ -295,12 +295,12 @@ class GameInterface(BaseGameInterface):
         character_base_id = self.load_data_when_available('_mantella_current_actor_base_id', '')
         character_ref_id = self.load_data_when_available('_mantella_current_actor_ref_id', '')
         if (character_base_id == '0' and character_ref_id == '0') or (character_base_id == '' and character_ref_id == ''): # if character ID is 0 or empty, check old id file for refid
-            with open(f'{self.game_path}/_mantella_current_actor_id.txt', 'r') as f:
+            with open(f'{self.game_path}\\_mantella_current_actor_id.txt', 'r') as f:
                 character_id = f.readline().strip()
             character_ref_id = character_id
             character_base_id = None # No base ID available
         time.sleep(0.5) # wait for file to register
-        with open(f'{self.game_path}/_mantella_current_actor.txt', 'r') as f:
+        with open(f'{self.game_path}\\_mantella_current_actor.txt', 'r') as f:
             character_name = f.readline().strip()
         
         return character_name, character_ref_id, character_base_id
@@ -327,7 +327,7 @@ class GameInterface(BaseGameInterface):
     def get_current_context_string(self):
         """Wait for context string to populate"""
         
-        with open(f'{self.game_path}/_mantella_context_string.txt', 'r', encoding='utf-8') as f:
+        with open(f'{self.game_path}\\_mantella_context_string.txt', 'r', encoding='utf-8') as f:
             context_string = f.readline().strip()
         return context_string
     
@@ -349,7 +349,7 @@ class GameInterface(BaseGameInterface):
         delay_between_attempts = 1
         for attempt in range(max_attempts):
             try:
-                with open(f'{self.game_path}/_mantella_actor_methods.txt', 'a', encoding='utf-8') as f:
+                with open(f'{self.game_path}\\_mantella_actor_methods.txt', 'a', encoding='utf-8') as f:
                     f.write(f'{function_call}\n')
                 break
             except PermissionError:
@@ -360,17 +360,17 @@ class GameInterface(BaseGameInterface):
                     time.sleep(delay_between_attempts)
 
     def is_radiant_dialogue(self):
-        with open(f'{self.game_path}/_mantella_radiant_dialogue.txt', 'r', encoding='utf-8') as f: # check if radiant dialogue is enabled
+        with open(f'{self.game_path}\\_mantella_radiant_dialogue.txt', 'r', encoding='utf-8') as f: # check if radiant dialogue is enabled
             radiant_dialogue = f.readline().strip().lower()
         return radiant_dialogue == 'true'
 
     def is_conversation_ended(self):
-        with open(f'{self.game_path}/_mantella_end_conversation.txt', 'r', encoding='utf-8') as f: # check if conversation has ended
+        with open(f'{self.game_path}\\_mantella_end_conversation.txt', 'r', encoding='utf-8') as f: # check if conversation has ended
             conversation_ended = f.readline().strip().lower()
         return conversation_ended == 'true'
     
     def load_ingame_actor_count(self):
-        with open(f'{self.game_path}/_mantella_actor_count.txt', 'r', encoding='utf-8') as f: # check how many characters are in the conversation
+        with open(f'{self.game_path}\\_mantella_actor_count.txt', 'r', encoding='utf-8') as f: # check how many characters are in the conversation
             try:
                 num_characters_selected = int(f.readline().strip())
             except:
@@ -585,8 +585,8 @@ class GameInterface(BaseGameInterface):
     
     def check_mic_status(self):
         """Check if the microphone is enabled in the MCM"""
-        if os.path.exists(f'{self.config.game_path}/_mantella_microphone_enabled.txt'):
-            with open(f'{self.config.game_path}/_mantella_microphone_enabled.txt', 'r', encoding='utf-8') as f:
+        if os.path.exists(f'{self.config.game_path}\\_mantella_microphone_enabled.txt'):
+            with open(f'{self.config.game_path}\\_mantella_microphone_enabled.txt', 'r', encoding='utf-8') as f:
                 mcm_mic_enabled = f.readline().strip()
             return mcm_mic_enabled == 'TRUE'
         else:   
@@ -597,7 +597,7 @@ class GameInterface(BaseGameInterface):
         """Add in-game events to player's response"""
 
         # append in-game events to player's response
-        with open(f'{self.game_path}/_mantella_in_game_events.txt', 'r', encoding='utf-8') as f:
+        with open(f'{self.game_path}\\_mantella_in_game_events.txt', 'r', encoding='utf-8') as f:
             in_game_events_lines = f.readlines()[-5:] # read latest 5 events
         
         # Is Player in combat with NPC
