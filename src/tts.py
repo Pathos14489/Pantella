8c411c37b78483fa56575bc1b8cf959cd32956e0
Pathos <29135741+Pathos14489@users.noreply.github.com>
2024-08-07 15:11:54 -0700

    cleaned up tts.py

--- src/tts.py
+++ src/tts.py
@@ -7,9 +7,9 @@ logging.info("Imported required libraries in tts.py")
 with open(os.path.join(os.path.dirname(__file__), "module_banlist"), "r") as f:
     banned_modules = f.read().split("\n")
 
-default = "xvasynth" # The default LLM to use if the one specified in config.json is not found or if default is specified in config.json
+default = "xvasynth" # The default TTS to use if the one specified in config.json is not found or if default is specified in config.json
 tts_Types = {}
-# Get all LLMs from src/llms/ and add them to LLM_Types
+# Get all TTSes from src/ttses/ and add them to TTS_Types
 for file in os.listdir(os.path.join(os.path.dirname(__file__), "tts_types/")):
     if file.endswith(".py") and not file.startswith("__"):
         module_name = file[:-3]
@@ -25,34 +25,34 @@ logging.info("Imported TTS types in tts.py")
 # print available TTS types
 logging.config(f"Available TTS types: {tts_Types.keys()}")
 
-# Create LLM object using the config and client provided
+# Create TTS object using the config and client provided
     
 def create_Synthesizer(conversation_manager):
-    slug = conversation_manager.config.tts_engine
-    if type(slug) == list and len(slug) == 1:
-        slug = slug[0]
+    slugs = conversation_manager.config.tts_engine # Get the TTS slug from config.json
+    if type(slugs) == list and len(slugs) == 1:
+        slugs = slugs[0]
     ttses = []
-    if type(slug) == list:
-        for s in slug:
-            if s not in tts_Types:
-                s = s.lower()
-            if s not in tts_Types:
-                logging.error(f"Could not find inference engine: {s}! Please check your config.json file and try again!")
+    if type(slugs) == list: # If there are multiple TTS engines specified in config.json
+        for slug in slugs:
+            if slug not in tts_Types:
+                slug = slug.lower()
+            if slug not in tts_Types:
+                logging.error(f"Could not find inference engine: {slug}! Please check your config.json file and try again!")
                 input("Press enter to continue...")
-                raise ValueError(f"Could not find inference engine: {s}! Please check your config.json file and try again!")
-            synth = tts_Types[s].Synthesizer(conversation_manager)
+                raise ValueError(f"Could not find inference engine: {slug}! Please check your config.json file and try again!")
+            synth = tts_Types[slug].Synthesizer(conversation_manager)
             synth.crashable = False # If we have multiple tts engines, we don't want to crash if one of them doesn't have a voice model
             ttses.append(synth)
         return tts_Types["multi_tts"].Synthesizer(conversation_manager, ttses)
-    elif type(slug) == str:
-        if slug not in tts_Types:
-            slug = slug.lower()
-        if slug not in tts_Types:
+    elif type(slugs) == str: # If there is only one TTS engine specified in config.json
+        if slugs not in tts_Types:
+            slugs = slugs.lower()
+        if slugs not in tts_Types:
             logging.error(f"Could not find inference engine: {conversation_manager.config.tts_engine}! Please check your config.json file and try again!")
             input("Press enter to continue...")
             raise ValueError(f"Could not find inference engine: {conversation_manager.config.tts_engine}! Please check your config.json file and try again!")
-        return tts_Types[slug].Synthesizer(conversation_manager)
+        return tts_Types[slugs].Synthesizer(conversation_manager)
     else:
-        logging.error(f"Wrong type for tts_engine in config.json! Expected string or list of strings, got '{type(slug)}'! Please check your config.json file and try again!")
+        logging.error(f"Wrong type for tts_engine in config.json! Expected string or list of strings, got '{type(slugs)}'! Please check your config.json file and try again!")
         input("Press enter to continue...")
-        raise ValueError(f"Wrong type for tts_engine in config.json! Expected string or list of strings, got '{type(slug)}'! Please check your config.json file and try again!")
+        raise ValueError(f"Wrong type for tts_engine in config.json! Expected string or list of strings, got '{type(slugs)}'! Please check your config.json file and try again!")
