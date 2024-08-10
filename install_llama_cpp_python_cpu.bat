e28f81f848521cb5fff50cde9b6c46b6b8e0f7d6
Pathos <29135741+Pathos14489@users.noreply.github.com>
2024-08-09 17:18:02 -0700

    got rid of batch files that have long been included with the launcher I just forgot to remove them

--- install_llama_cpp_python_cpu.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set CMAKE_ARGS=""
-set FORCE_CMAKE=0
-echo Installing llama-cpp-python for CPU
-python3 -m pip install llama-cpp-python --no-deps --no-cache-dir --force-reinstall --upgrade
-echo Done
