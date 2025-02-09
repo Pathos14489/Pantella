3732c1c28d7582ac2fdc7f96150280f87f68ed40
Pathos <29135741+Pathos14489@users.noreply.github.com>
2025-02-09 15:42:17 -0800

    added extras tokenizer for my API and I think for another API but I can't remember where I borrowed the endpoint standard from ¯\_(ツ)_/¯

---
+++ src/tokenizers/extras.py
@@ -0,0 +1,27 @@
+print("Importing koboldcpp.py...")
+from src.logging import logging
+import src.utils as utils
+import src.tokenizers.base_tokenizer as tokenizer
+import requests
+logging.info("Imported required libraries in koboldcpp.py")
+
+tokenizer_slug = "extras"
+class Tokenizer(tokenizer.base_Tokenizer): # Gets token count from OpenAI's embedding API -- WARNING SLOW AS HELL -- Only use if you don't want to set up the right tokenizer for your local model or if you don't know how to do that
+    def __init__(self, conversation_manager, client):
+        super().__init__(conversation_manager)
+        if not (self.config.inference_engine == "openai" or self.config.inference_engine == "default"):
+            logging.error(f"koboldcpp tokenizer only works using OpenAI's API! Please check your config.json file and try again!")
+            input("Press enter to continue...")
+            raise ValueError(f"koboldcpp tokenizer only works using OpenAI's API! Please check your config.json file and try again!")
+        self.tokenizer_slug = tokenizer_slug
+        self.client = client # Unnecessary for this tokenizer, but it's here for compatibility with other openai tokenizers
+        
+    @utils.time_it
+    def get_token_count(self, string):
+        """Returns the number of tokens in the string"""
+        url = self.config.alternative_openai_api_base.replace("/v1","") + "/extras/tokenize/count"
+        data = {"input": string, "model": self.config.openai_model}
+        r = requests.post(url, json=data).text
+        num_tokens = int(r)
+        return num_tokens
+    
