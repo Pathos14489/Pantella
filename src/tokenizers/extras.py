32e87697249b6f2c26eee16d98f83f8ee2c6c797
Pathos <29135741+Pathos14489@users.noreply.github.com>
2026-04-26 23:22:40 -0700

    fixed other tokenizer types for openai_api from the old slug to the new slug

--- src/tokenizers/extras.py
+++ src/tokenizers/extras.py
@@ -9,7 +9,7 @@ tokenizer_slug = "extras"
 class Tokenizer(tokenizer.base_Tokenizer): # Gets token count from OpenAI's embedding API -- WARNING SLOW AS HELL -- Only use if you don't want to set up the right tokenizer for your local model or if you don't know how to do that
     def __init__(self, conversation_manager, client):
         super().__init__(conversation_manager, tokenizer_slug)
-        if not (self.config.inference_engine == "openai" or self.config.inference_engine == "default"):
+        if not (self.config.inference_engine == "openai_api" or self.config.inference_engine == "default"):
             logging.error(f"extras tokenizer only works using a OpenAI API! Please check your config.json file and try again!")
             input("Press enter to continue...")
             raise ValueError(f"extras tokenizer only works using a OpenAI API! Please check your config.json file and try again!")
--- src/tokenizers/openai_embedding_tokenizer.py
+++ src/tokenizers/openai_embedding_tokenizer.py
@@ -8,7 +8,7 @@ tokenizer_slug = "embedding"
 class Tokenizer(tokenizer.base_Tokenizer): # Gets token count from OpenAI's embedding API -- WARNING SLOW AS HELL -- Only use if you don't want to set up the right tokenizer for your local model or if you don't know how to do that
     def __init__(self, conversation_manager, client):
         super().__init__(conversation_manager)
-        if not (self.config.inference_engine == "openai" or self.config.inference_engine == "default"):
+        if not (self.config.inference_engine == "openai_api" or self.config.inference_engine == "default"):
             logging.error(f"Embedding tokenizer only works with OpenAI's API! Please check your config.json file and try again!")
             input("Press enter to continue...")
             raise ValueError(f"Embedding tokenizer only works with OpenAI's API! Please check your config.json file and try again!")
